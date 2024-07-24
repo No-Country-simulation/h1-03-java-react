9c57274c2f33d904873eb32c191252eb653f2cba
DavidLG89 <thomendavid1@gmail.com>
2024-07-24 02:03:17 -0300

    Add: list of user roles in login response

--- _BACKEND/src/main/java/com/no_country/justina/service/implementation/LoginService.java
+++ _BACKEND/src/main/java/com/no_country/justina/service/implementation/LoginService.java
@@ -2,15 +2,20 @@ package com.no_country.justina.service.implementation;
 
 import com.no_country.justina.model.dto.LoginReq;
 import com.no_country.justina.model.dto.LoginRes;
+import com.no_country.justina.model.dto.RoleRes;
 import com.no_country.justina.model.entities.UserEntity;
 import com.no_country.justina.repository.UserRepository;
 import com.no_country.justina.service.interfaces.ILoginService;
 import com.no_country.justina.util.JWTUtils;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class LoginService implements ILoginService {
     private final JWTUtils jwtUtils;
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public LoginRes authenticate(LoginReq loginReq) {
@@ -30,6 +36,7 @@ public class LoginService implements ILoginService {
         );
         UserEntity user = userRepository.findByEmail(loginReq.getEmail()).orElseThrow(() -> new EntityNotFoundException("El correo ingresado no es valido"));
         var token = jwtUtils.generateToken(user);
-        return new LoginRes(token);
+        Set<RoleRes> rolesRes = user.getRoles().stream().map(role -> modelMapper.map(role, RoleRes.class)).collect(Collectors.toSet());
+        return new LoginRes(token, rolesRes);
     }
 }
