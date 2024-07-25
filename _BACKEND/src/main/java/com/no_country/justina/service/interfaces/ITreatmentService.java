77e989dd75b331aa59358fe4f39f6cffb8aacbbd
Dexametasona <machicadogomezalexander@gmail.com>
2024-07-25 10:34:36 -0500

    feat:adding relation to treatment entity, and adding filters

--- _BACKEND/src/main/java/com/no_country/justina/service/interfaces/ITreatmentService.java
+++ _BACKEND/src/main/java/com/no_country/justina/service/interfaces/ITreatmentService.java
@@ -4,10 +4,20 @@ import com.no_country.justina.model.entities.Treatment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
+
 public interface ITreatmentService {
   Treatment create(Treatment treatment);
   Treatment getById(Long id);
   Page<Treatment> getAll(Pageable pageable);
+
+  Page<Treatment> getAllByFilters(Pageable pageable,
+                                  Long doctorId,
+                                  Long historyId,
+                                  Long specialtyId,
+                                  LocalDateTime start,
+                                  LocalDateTime end);
+
   Treatment update(Treatment treatment);
   void deleteById(Long id);
 }
