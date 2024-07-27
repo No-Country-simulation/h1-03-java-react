e7ceb6ccd7d8450ab2247d2fed6616688955480d
DavidLG89 <thomendavid1@gmail.com>
2024-07-27 13:22:55 -0300

    Add: user-patient get method in controller

--- _BACKEND/src/main/java/com/no_country/justina/controller/UserAndPatientController.java
+++ _BACKEND/src/main/java/com/no_country/justina/controller/UserAndPatientController.java
@@ -1,6 +1,8 @@
 package com.no_country.justina.controller;
 
+import com.no_country.justina.model.dto.UserAndDoctorRes;
 import com.no_country.justina.model.dto.UserAndPatientReq;
+import com.no_country.justina.model.dto.UserAndPatientRes;
 import com.no_country.justina.model.dto.UserRes;
 import com.no_country.justina.model.entities.Patient;
 import com.no_country.justina.model.entities.UserEntity;
@@ -12,10 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,5 +36,10 @@ public class UserAndPatientController {
                 modelMapper.map(userAndPatientReq.getPatient(), Patient.class));
 
         return new ResponseEntity<>(modelMapper.map(user, UserRes.class), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<UserAndPatientRes> getUserAndDoctor() {
+        return new ResponseEntity<>(userAndPatientService.get(), HttpStatus.OK);
     }
 }
