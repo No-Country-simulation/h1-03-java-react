318be83cbd4aa0dcca98f13e461b85329ba0bb90
FlorenciaNieva <florencianieva930@gmail.com>
2024-07-31 12:35:48 -0300

    Creation of MedicalLinks component with hardcoded information

---
+++ _FRONTEND/src/components/DashboardComponents/DashboardDoctor/MedicalLinks/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import medicalAppointment from "../../../../assets/svg/icons/medicalAppointmentDashboard.svg";
+import clinicalHistories from "../../../../assets/svg/icons/clinicalHistoriesDashboard.svg";
+import appointments from "../../../../assets/svg/icons/appointmentsDashboard.svg";
+
+export default function index() {
+  return (
+    <div className="flex md:flex-row justify-center items-center p-4 w-full h-auto md:h-[125px] gap-8">
+      <div className="flex flex-row justify-center flex-1 gap-8">
+        <div className="flex flex-col justify-center items-center p-4 gap-6">
+          <img
+            src={medicalAppointment}
+            alt=""
+            aria-label=""
+            title=""
+            width={100}
+            height={100}
+            loading="lazy"
+          />
+          <p className="text-center">Turnos Medicos</p>
+        </div>
+        <div className="flex flex-col items-center justify-center p-4 gap-6">
+          <img
+            src={clinicalHistories}
+            alt=""
+            aria-label=""
+            title=""
+            width={100}
+            height={100}
+            loading="lazy"
+          />
+          <p className="text-center">Historias clínicas</p>
+        </div>
+        <div className="flex flex-col items-center justify-center p-4 gap-6">
+          <img
+            src={appointments}
+            alt=""
+            aria-label=""
+            title=""
+            width={100}
+            height={100}
+            loading="lazy"
+          />
+          <p className="text-center">Turnos</p>
+        </div>
+      </div>
+    </div>
+  );
+}
