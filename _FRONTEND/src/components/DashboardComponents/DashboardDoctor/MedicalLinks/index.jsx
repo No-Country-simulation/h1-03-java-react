bb86deeb1022bf7d65fc1ee5a279d296a610ef5b
FlorenciaNieva <florencianieva930@gmail.com>
2024-07-31 15:28:31 -0300

    Creation of component to reuse code.

--- _FRONTEND/src/components/DashboardComponents/DashboardDoctor/MedicalLinks/index.jsx
+++ _FRONTEND/src/components/DashboardComponents/DashboardDoctor/MedicalLinks/index.jsx
@@ -2,47 +2,15 @@ import React from "react";
 import medicalAppointment from "../../../../assets/svg/icons/medicalAppointmentDashboardDoctor.svg";
 import clinicalHistories from "../../../../assets/svg/icons/clinicalHistoriesDashboardDoctor.svg";
 import appointments from "../../../../assets/svg/icons/appointmentsDashboardDoctor.svg";
+import ItemLink from "./ItemLink"
 
 export default function index() {
   return (
     <div className="flex md:flex-row justify-center items-center p-4 w-full h-auto md:h-[125px] gap-8">
       <div className="flex flex-row justify-center flex-1 gap-8">
-        <div className="flex flex-col justify-center items-center p-4 gap-6">
-          <img
-            src={medicalAppointment}
-            alt=""
-            aria-label=""
-            title=""
-            width={100}
-            height={100}
-            loading="lazy"
-          />
-          <p className="text-center">Turnos Medicos</p>
-        </div>
-        <div className="flex flex-col items-center justify-center p-4 gap-6">
-          <img
-            src={clinicalHistories}
-            alt=""
-            aria-label=""
-            title=""
-            width={100}
-            height={100}
-            loading="lazy"
-          />
-          <p className="text-center">Historias clínicas</p>
-        </div>
-        <div className="flex flex-col items-center justify-center p-4 gap-6">
-          <img
-            src={appointments}
-            alt=""
-            aria-label=""
-            title=""
-            width={100}
-            height={100}
-            loading="lazy"
-          />
-          <p className="text-center">Turnos</p>
-        </div>
+        <ItemLink icon={medicalAppointment} title="Turnos Medicos" />
+        <ItemLink icon={clinicalHistories} title="Historias clínicas" />
+        <ItemLink icon={appointments} title="Turnos" />
       </div>
     </div>
   );
