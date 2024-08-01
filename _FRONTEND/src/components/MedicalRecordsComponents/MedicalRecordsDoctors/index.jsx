02337425801ce0b28e573adccfd5f87a9c5171b4
FlorenciaNieva <florencianieva930@gmail.com>
2024-08-01 20:05:16 -0300

    Incorporation of SearchBar, MedicalHistorySearch and Pagination components to the MedicalRecordsDoctors

--- _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsDoctors/index.jsx
+++ _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsDoctors/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
+import Searchbar from "./Searchbar";
+import MedicalHistorySearch from "./MedicalHistorySearch"
+import Pagination from "./Pagination"
 
-export default function MedicalRecordsDoctors() {    
-
-    return (
-        <>
-            medical records doctors
-        </>
-    )
+export default function index() {
+  return (
+    <section className="min-h-screen flex flex-col justify-center items-start gap-20 p-10 pt-10">
+      <div className="w-full lg:flex-1">
+        <Searchbar />
+        <MedicalHistorySearch />
+        <Pagination totalPages={20} />
+      </div>
+    </section>
+  );
 }
