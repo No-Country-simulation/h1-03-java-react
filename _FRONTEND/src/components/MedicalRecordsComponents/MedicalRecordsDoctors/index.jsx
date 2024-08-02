9835ecbee91972ed0a1bf361c06c82764a2b9042
FlorenciaNieva <florencianieva930@gmail.com>
2024-08-01 22:13:30 -0300

    Modify the name of the component

--- _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsDoctors/index.jsx
+++ _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsDoctors/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Searchbar from "./Searchbar";
-import MedicalHistorySearch from "./MedicalHistorySearch"
+import MedicalHistoryResult from "./MedicalHistoryResult"
 import Pagination from "./Pagination"
 
 export default function index() {
@@ -8,7 +8,7 @@ export default function index() {
     <section className="min-h-screen flex flex-col justify-center items-start gap-20 p-10 pt-10">
       <div className="w-full lg:flex-1">
         <Searchbar />
-        <MedicalHistorySearch />
+        <MedicalHistoryResult />
         <Pagination totalPages={20} />
       </div>
     </section>
