976a6a4f3f52b04f077efdf2d7b26c89be851423
FlorenciaNieva <florencianieva930@gmail.com>
2024-08-16 15:50:56 -0300

    Adding language change to MedicalConsultationComponents component

--- _FRONTEND/src/components/AppointmentsComponents/AppointmentsDoctors/MedicalConsultationComponents/AppointmentResult/ItemAppointment/index.jsx
+++ _FRONTEND/src/components/AppointmentsComponents/AppointmentsDoctors/MedicalConsultationComponents/AppointmentResult/ItemAppointment/index.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import calendar from "../../../../../../assets/svg/others/calendarReserveAppointment.svg";
 import search from "../../../../../../assets/svg/others/search.svg";
 import AppointmentReviewPopUp from "./AppointmentReviewPopUp";
+import { useSelector } from "react-redux";
+import i18n from "../../../../../../i18n/appointmentsDoctor/medicalConsultation/index.json";
 
 export default function index({ date, name, profession, timeSlot }) {
+  const language = useSelector((state) => state.i18nReducer.language);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
@@ -24,9 +27,9 @@ export default function index({ date, name, profession, timeSlot }) {
         <div className="flex justify-center items-center">
           <img
             src={calendar}
-            alt="icono de historial clinico"
-            aria-label=""
-            title=""
+            alt={i18n[language].icons.calendar}
+            aria-label={i18n[language].icons.calendar}
+            title={i18n[language].icons.calendar}
             width={50}
             height={50}
             loading="lazy"
@@ -41,22 +44,22 @@ export default function index({ date, name, profession, timeSlot }) {
         <p className="text-2xl">{profession}</p>
       </div>
       <div className="flex flex-col flex-1 items-center gap-4 p-4">
-        <p className="text-xl">Franja horaria</p>
+        <p className="text-xl">{i18n[language].itemResult.timeSlot}</p>
         <div className="flex justify-center border-2 border-[#5666BE] rounded-full">
           <div
             className={`flex-1 px-10 py-4 rounded-l-full ${
               timeSlot === "morning" ? "bg-[#e8def8]" : ""
             } `}
           >
-            <p className="text-[#1D192B] font-medium text-sm">Mañana</p>
+            <p className="text-[#1D192B] font-medium text-sm">{i18n[language].itemResult.morning}</p>
           </div>
           <div className="w-0.5 bg-[#5666BE] h-full"></div>
           <div
             className={`flex-1 px-10 py-4 rounded-r-full ${
-              timeSlot === "afternoom" ? "bg-[#e8def8]" : ""
+              timeSlot === "afternoon" ? "bg-[#e8def8]" : ""
             } `}
           >
-            <p className="text-[#1D192B] font-medium text-sm">Tarde</p>
+            <p className="text-[#1D192B] font-medium text-sm">{i18n[language].itemResult.afternoon}</p>
           </div>
         </div>
       </div>
@@ -64,13 +67,13 @@ export default function index({ date, name, profession, timeSlot }) {
         className="flex flex-col flex-1 items-center gap-4 p-4 cursor-pointer"
         onClick={openModal}
       >
-        <p className="text-xl">Revisar turnos</p>
+        <p className="text-xl">{i18n[language].itemResult.reviewAppointments}</p>
         <div className="flex justify-center">
           <img
             src={search}
-            alt="icono de busqueda"
-            aria-label=""
-            title=""
+            alt={i18n[language].icons.search}
+            aria-label={i18n[language].icons.search}
+            title={i18n[language].icons.search}
             width={40}
             height={40}
             loading="lazy"
