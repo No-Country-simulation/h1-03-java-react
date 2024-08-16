ab61378afb3107dc277d7a2c3b91ccad0eeca335
FlorenciaNieva <florencianieva930@gmail.com>
2024-08-16 19:10:31 -0300

    Modifying

--- _FRONTEND/src/components/DashboardComponents/DashboardDoctor/MedicalLinks/index.jsx
+++ _FRONTEND/src/components/DashboardComponents/DashboardDoctor/MedicalLinks/index.jsx
@@ -14,21 +14,21 @@ export default function index() {
       <div className="flex flex-col sm:flex-row justify-center flex-1 gap-4 sm:gap-8">
         <ItemLink
           icon={medicalAppointment}
-          alt={i18n[language].itemsLinks.appointmentMedical.alt}
+          alt={i18n[language].itemsLinks.appointmentMedical.icon}
           title={i18n[language].itemsLinks.appointmentMedical.title}
-          path="/appointments"
+          path={i18n[language].itemsLinks.appointmentMedical.path}
         />
         <ItemLink
           icon={clinicalHistories}
-          alt={i18n[language].itemsLinks.medicalRecords.alt}
+          alt={i18n[language].itemsLinks.medicalRecords.icon}
           title={i18n[language].itemsLinks.medicalRecords.title}
-          path="/medical-records/search-medical-records"
+          path={i18n[language].itemsLinks.medicalRecords.path}
         />
         <ItemLink
           icon={appointments}
-          alt={i18n[language].itemsLinks.appointments.alt}
+          alt={i18n[language].itemsLinks.appointments.icon}
           title={i18n[language].itemsLinks.appointments.title}
-          path="/appointments/my-appointments"
+          path={i18n[language].itemsLinks.appointments.path}
         />
       </div>
     </div>
