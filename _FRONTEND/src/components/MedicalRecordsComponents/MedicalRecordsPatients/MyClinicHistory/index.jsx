464b21a38c660117ffd981504610fc8f68cdd1f2
FlorenciaNieva <florencianieva930@gmail.com>
2024-08-19 20:08:13 -0300

    Adding language change to MedicalRecordPatients component

--- _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsPatients/MyClinicHistory/index.jsx
+++ _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsPatients/MyClinicHistory/index.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import ItemTreatament from "../../MedicalRecordsDoctors/TreatmentSheet/ItemTreatment"
-import myClinicHistory from "../../../../assets/svg/others/myClinicHistory.svg"
+import React from "react";
+import ItemTreatament from "../../MedicalRecordsDoctors/TreatmentSheet/ItemTreatment";
+import myClinicHistory from "../../../../assets/svg/others/myClinicHistory.svg";
+import { useSelector } from "react-redux";
+import i18n from "../../../../i18n/medicalRecordPatient/index.json";
 
 export default function index() {
+  const language = useSelector((state) => state.i18nReducer.language);
+
   return (
     <div className="flex flex-col py-4 justify-center items-center w-full h-auto gap-4 text-black">
-      <div className='flex items-center gap-4'>
-        <img src={myClinicHistory} alt="icono de historia clinica" />
-        <p>Mi Historia Clínica</p>
+      <div className="flex items-center gap-4">
+        <img
+          src={myClinicHistory}
+          alt={i18n[language].myClinicHistory.icon}
+          aria-label={i18n[language].myClinicHistory.icon}
+          title={i18n[language].myClinicHistory.icon}
+        />
+        <p>{i18n[language].myClinicHistory.title}</p>
       </div>
 
       <ItemTreatament />
       <ItemTreatament />
     </div>
-  )
+  );
 }
--- _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsPatients/index.jsx
+++ _FRONTEND/src/components/MedicalRecordsComponents/MedicalRecordsPatients/index.jsx
@@ -4,8 +4,12 @@ import MyClinicHistory from "./MyClinicHistory";
 import Filters from "../MedicalRecordsDoctors/Filters";
 import Buttons from "../MedicalRecordsPatients/Buttons";
 import magnifyingGlass from "../../../assets/svg/others/magnifyingGlassPurple.svg";
+import { useSelector } from "react-redux";
+import i18n from "../../../i18n/medicalRecordPatient/index.json";
 
 export default function index() {
+  const language = useSelector((state) => state.i18nReducer.language);
+
   return (
     <section className="min-h-screen w-full p-16">
       <div className="flex flex-col justify-center items-start w-full gap-8">
@@ -16,11 +20,16 @@ export default function index() {
           </div>
           <div className="flex flex-col lg:w-1/4 py-4 gap-6 items-center">
             <div className="flex items-center gap-4">
-              <img src={magnifyingGlass} alt="icono lupa" />
-              <p>Buscar historia clínica</p>
+              <img
+                src={magnifyingGlass}
+                alt={i18n[language].search.icon}
+                aria-label={i18n[language].search.icon}
+                title={i18n[language].search.icon}
+              />
+              <p>{i18n[language].search.title}</p>
             </div>
-              <Filters />
-              <Buttons />
+            <Filters />
+            <Buttons />
           </div>
         </div>
       </div>
